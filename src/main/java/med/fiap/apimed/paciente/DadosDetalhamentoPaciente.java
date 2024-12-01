c8f0f157d461887a38371d34eff159420d19b3be
Lucas Cunha <lucas71cunha@gmail.com>
2024-12-01 00:11:08 -0300

    Padronizando retornos da Api

---
+++ src/main/java/med/fiap/apimed/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,9 @@
+package med.fiap.apimed.paciente;
+
+import med.fiap.apimed.endereco.Endereco;
+
+public record DadosDetalhamentoPaciente(String nome, String email, String telefone, String cpf, Endereco endereco) {
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(), paciente.getEndereco());
+    }
+}
